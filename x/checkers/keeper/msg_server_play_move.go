b6a1280b10b6ca0ed19c327d467e2243cd455211
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-08-23 14:18:46 +0900

    create logic to move player

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -2,16 +2,71 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
 	"github.com/alice/checkers/x/checkers/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	rules "github.com/alice/checkers/x/checkers/rules"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// playしているgameのデータを取得
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.IdValue)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "game not found %s", msg.IdValue)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	// 実行者がどのplayerかを判定してplayer変数に格納
+	isRed := strings.Compare(storedGame.Red, msg.Creator) == 0
+	isBlack := strings.Compare(storedGame.Black, msg.Creator) == 0
+	var player rules.Player
+	if !isRed && !isBlack {
+		return nil, types.ErrCreatorNotPlayer
+	} else if isRed && isBlack {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isRed {
+		player = rules.RED_PLAYER
+	} else {
+		player = rules.BLACK_PLAYER
+	}
+
+	// 現在、実行者のターンなのか判定
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+	if !game.TurnIs(player) {
+		return nil, types.ErrNotPlayerTurn
+	}
+
+	// Do it
+	captured, moveErr := game.Move(
+		rules.Pos{
+			X: int(msg.FromX),
+			Y: int(msg.FromY),
+		},
+		rules.Pos{
+			X: int(msg.ToX),
+			Y: int(msg.ToY),
+		},
+	)
+	if moveErr != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, moveErr.Error())
+	}
+
+	// Save for the next play move
+	storedGame.Game = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	// What to inform
+	return &types.MsgPlayMoveResponse{
+		IdValue:   msg.IdValue,
+		CapturedX: int64(captured.X),
+		CapturedY: int64(captured.Y),
+		Winner:    rules.PieceStrings[game.Winner()],
+	}, nil
 }
