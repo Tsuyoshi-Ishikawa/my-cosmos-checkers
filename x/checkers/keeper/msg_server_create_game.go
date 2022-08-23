ab880261dc534036a2d39ab67beeedd89311b7d7
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-08-23 11:47:48 +0900

    create logic to create new game

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,16 +2,47 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	rules "github.com/alice/checkers/x/checkers/rules"
 )
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// 新しいゲームのidを取得する
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+			panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	// 新しいゲームデータを作成する
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+			Creator: msg.Creator,
+			Index:   newIndex,
+			Game:    newGame.String(),
+			Turn:    rules.PieceStrings[newGame.Turn],
+			Red:     msg.Red,
+			Black:   msg.Black,
+	}
+
+	// 新しいゲームデータの検証
+	err := storedGame.Validate()
+	if err != nil {
+			return nil, err
+	}
+
+	// 新しいゲームデータの保存
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	// この次のゲームのidを設定するためにincrementして保存する
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
---
+++ x/checkers/types/stored_game_method.go
@@ -0,0 +1,59 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/alice/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// stored_game.pb.goは/protoのcompile結果なので直接いじるのは良くない
+// なのでstored_gameにfunctionを設定したい場合はこのようにfileを作って対応してあげる。
+// errorハンドリングはsdkerrorsで行い、エラーメッセージ(ErrInvalidCreatorなど)はx/checkers/types/errors.goに格納する
+// 参考：https://github.com/cosmos/b9-checkers-academy-draft/blob/main/x/checkers/types/full_game.go
+
+func (storedGame *StoredGame) GetCreatorAddress() (creator sdk.AccAddress, err error) {
+	creator, errCreator := sdk.AccAddressFromBech32(storedGame.Creator)
+	return creator, sdkerrors.Wrapf(errCreator, ErrInvalidCreator.Error(), storedGame.Creator)
+}
+
+func (storedGame *StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+}
+
+func (storedGame *StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+}
+
+func (storedGame *StoredGame) ParseGame() (game *rules.Game, err error) {
+	game, errGame := rules.Parse(storedGame.Game)
+	if errGame != nil {
+		return nil, sdkerrors.Wrapf(errGame, ErrGameNotParseable.Error())
+	}
+	game.Turn = rules.StringPieces[storedGame.Turn].Player
+	if game.Turn.Color == "" {
+		return nil, sdkerrors.Wrapf(errors.New(fmt.Sprintf("Turn: %s", storedGame.Turn)), ErrGameNotParseable.Error())
+	}
+	return game, nil
+}
+
+func (storedGame StoredGame) Validate() (err error) {
+	_, err = storedGame.GetCreatorAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.ParseGame()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.GetRedAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.GetBlackAddress()
+	return err
+}
